0733235942fac6ac47b7b046f6b7897360357517
Matic Jurglic <matic@jurglic.si>
2025-06-22 21:11:16 +0200

    Updates

--- src/app/components/UpcomingEvents.tsx
+++ src/app/components/UpcomingEvents.tsx
@@ -36,6 +36,30 @@ export default function UpcomingEvents() {
             </div>
           </div>
         </Link>
+
+        {/* Endemit Label Release Event */}
+        <div className="border-b border-[#333333] pt-4 pb-4">
+          <div>
+            <span className="text-xl font-bold text-white">
+              ENDEMIT LABEL RELEASE PARTY
+            </span>
+            <span className="text-gray-400 ml-3">
+              20 Sep 2025, Grad Kodeljevo
+            </span>
+            <span className="text-sm text-gray-400 ml-3">INLAND • MMALI</span>
+          </div>
+        </div>
+
+        {/* X Event */}
+        <div className="border-b border-[#333333] pt-4 pb-4">
+          <div>
+            <span className="text-xl font-bold text-white">TBA</span>
+            <span className="text-gray-400 ml-3">
+              21 Nov 2025, Grad Kodeljevo
+            </span>
+            <span className="text-sm text-gray-400 ml-3">PULSO • RHAEGAL</span>
+          </div>
+        </div>
       </div>
     </div>
   );
