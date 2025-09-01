f15f1d29a39476e0063389a8d01aba181a5d8ad1
Matic Jurglic <matic@jurglic.si>
2025-09-01 21:36:38 +0200

    Add stripe payment form

--- src/app/events/issun-boshi-vinyl-release/tickets/page.tsx
+++ src/app/events/issun-boshi-vinyl-release/tickets/page.tsx
@@ -13,14 +13,15 @@ export default function TicketsPage() {
       <div className="max-w-5xl space-y-6 p-5 lg:px-12 text-white font-typo flex-1 max-lg:pb-16">
         <Headline title="Tickets" />
 
-        <div className="text-2xl font-light mt-8">
-          Tickets will be available soon. Subscribe to our newsletter to stay
-          updated!
-        </div>
+        <script async src="https://js.stripe.com/v3/buy-button.js"></script>
 
-        <div className="bg-black/30 px-10 py-1 rounded-lg flex justify-center border-gray-900 border-2">
-          <EndemitSubscribe />
-        </div>
+        {/* @ts-ignore */}
+        <stripe-buy-button
+          buy-button-id="buy_btn_1S2d84Ir2nqko6q4Uq8sSKOe"
+          publishable-key="pk_live_51P6Vo2Ir2nqko6q44ic0fwaGZJjuheAWZfNOeekvDWZiRifEcTOHhJ023IoMWrrxCWXwPUUUmhlkqudmOrKw0qxG005H4cBkj3"
+        >
+          {/* @ts-ignore */}
+        </stripe-buy-button>
       </div>
       <CoverFooter />
     </>
